ec548ed6c8b7d25ab50c9371d1caf13c6173673b
OlgaDobrodey <olga.gurkovskaya@gmail.com>
2021-10-14 01:48:49 +0300

    added first commit

--- src/main/java/com/itrex/java/lab/service/ParamConnectionBase.java
+++ src/main/java/com/itrex/java/lab/service/ParamConnectionBase.java
@@ -1,8 +1,7 @@
 package com.itrex.java.lab.service;
 
 public class ParamConnectionBase {
-    public final static String DRIVER = "org.h2.Driver";
-    public final static String URL = "jdbc:h2:mem:db;DB_CLOSE_DELAY=-1";
+    public final static String URL = "jdbc:h2:./db/crm";
     public final static String USER = "sa";
     public final static String PSW = "";
 }
