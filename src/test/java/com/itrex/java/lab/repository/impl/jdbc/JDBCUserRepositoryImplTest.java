63ae7d321bfe730f0b9c239bcc5126f36abfecf5
OlgaDobrodey <olga.gurkovskaya@gmail.com>
2021-11-04 13:03:35 +0300

    added Spring dependens 3.0

--- src/test/java/com/itrex/java/lab/repository/impl/jdbc/JDBCUserRepositoryImplTest.java
+++ src/test/java/com/itrex/java/lab/repository/impl/jdbc/JDBCUserRepositoryImplTest.java
@@ -3,13 +3,15 @@ package com.itrex.java.lab.repository.impl.jdbc;
 import com.itrex.java.lab.repository.AbstractUserRepositoryTest;
 import com.itrex.java.lab.repository.TaskRepository;
 import com.itrex.java.lab.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 public class JDBCUserRepositoryImplTest extends AbstractUserRepositoryTest {
 
+    @Autowired
     public JDBCUserRepositoryImplTest(@Qualifier(value = "JDBCTaskRepository") TaskRepository taskRepository,
                                       @Qualifier(value = "JDBCUserRepository") UserRepository userRepository) {
         super();
-        postConstruct(userRepository,taskRepository);
+        postConstruct(userRepository, taskRepository);
     }
 }
