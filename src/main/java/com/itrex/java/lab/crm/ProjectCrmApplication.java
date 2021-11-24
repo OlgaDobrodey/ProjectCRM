2984c59db6c4fcb792082aa79131e0378742fa89
OlgaDobrodey <olga.gurkovskaya@gmail.com>
2021-11-25 00:01:00 +0300

    added method getByLogin() and singIn

--- src/main/java/com/itrex/java/lab/crm/ProjectCrmApplication.java
+++ src/main/java/com/itrex/java/lab/crm/ProjectCrmApplication.java
@@ -1,12 +1,6 @@
 package com.itrex.java.lab.crm;
 
-import com.itrex.java.lab.crm.repository.TaskRepository;
-import com.itrex.java.lab.crm.repository.UserRepository;
-import com.itrex.java.lab.crm.service.TaskService;
-import com.itrex.java.lab.crm.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -14,27 +8,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Slf4j
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class ProjectCrmApplication implements CommandLineRunner {
-
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    TaskRepository taskRepository;
-    @Autowired
-    TaskService taskService;
-    @Autowired
-    UserService userService;
+public class ProjectCrmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectCrmApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("===================START APP======================");
-        userRepository.selectByLogin("Petrov");
-        userRepository.selectByLogin("Ivanov@#");
-        System.out.println("=================SHUT DOWN APP====================");
     }
 
 }
